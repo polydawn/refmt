d1a402ea040ed7c85a52ba2e48d496c0f4626515
Eric Myhre <hash@exultant.us>
2016-10-02 18:41:37 -0700

    Tests for mapper walking working, using the token destination.
    ... and add selfie symlink, for ye who like to set a project-local $GOPATH.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- mapper_test.go
+++ mapper_test.go
@@ -1,7 +1,10 @@
 package xlate
 
 import (
+	"reflect"
 	"testing"
+
+	"github.com/polydawn/go-xlate/dest/tok"
 )
 
 func TestMapperSetup(t *testing.T) {
@@ -63,19 +66,66 @@ func TestMapperBasics(t *testing.T) {
 	}
 
 	type testRow struct {
-		title  string
-		thing  interface{}
-		mapper *Mapper
-		expect string
+		title     string
+		thing     interface{}
+		mapper    *Mapper
+		expect    tok.Tokens
+		expectErr error
 	}
 	for _, tr := range []testRow{{
 		title: "test a single thing at its zero value",
+		thing: BB{},
+		mapper: NewMapper(MapperSetup{
+			{BB{}, Map_Wildcard_toString},
+		}),
+		expect: tok.Tokens{
+			{tok.TokenKind_ValString, "{}"},
+		},
+	}, {
+		title: "test a single thing that has no handler",
 		thing: AA{},
 		mapper: NewMapper(MapperSetup{
-			{BB{}, Map_Wildcard_toStringOfType},
+			{BB{}, Map_Wildcard_toString},
 		}),
-		expect: "BB",
+		expectErr: &ErrMissingMappingFunc{reflect.TypeOf(AA{})},
+	}, {
+		title: "test a custom func plus recursion at its zero value",
+		thing: AA{},
+		mapper: NewMapper(MapperSetup{
+			// a fairly bizarre hack of recursion, trying to avoid triggering anything too fancy yet.
+			{AA{}, func(dispatch *Mapper, dest Destination, input interface{}) {
+				dest.OpenMap()
+				dest.WriteMapKey("type")
+				dest.WriteString("AA")
+				dest.WriteMapKey("data")
+				dispatch.Map(dest, input.(AA).Y)
+				dest.CloseMap()
+			}},
+			{BB{}, Map_Wildcard_toString},
+		}),
+		expect: tok.Tokens{
+			{tok.TokenKind_OpenMap, nil},
+			{tok.TokenKind_MapKey, "type"},
+			{tok.TokenKind_ValString, "AA"},
+			{tok.TokenKind_MapKey, "data"},
+			{tok.TokenKind_ValString, "{}"},
+			{tok.TokenKind_CloseMap, nil},
+		},
 	}} {
-		_ = tr
+		tokDest := tok.NewDestination()
+		err := capturePanics(func() {
+			tr.mapper.Map(
+				tokDest,
+				tr.thing,
+			)
+		})
+		if !stringyEquality(tr.expectErr, err) {
+			t.Errorf("test %q FAILED:\n\texpected     %#v\n\terrored with %#v",
+				tr.title, tr.expectErr, err)
+		}
+		if !stringyEquality(tr.expect, tokDest.Tokens) {
+			t.Errorf("test %q FAILED:\n\texpected: %#v\n\tactual:   %#v",
+				tr.title, tr.expect, tokDest.Tokens)
+		}
 	}
 }
