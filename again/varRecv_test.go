9e77fb1374b4aeb959c7f2b75b03b91665f8bf31
Eric Myhre <hash@exultant.us>
2016-10-29 00:29:13 -0700

    Tweak test helpers to be a little more one-liner-friendly.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- again/varRecv_test.go
+++ again/varRecv_test.go
@@ -8,26 +8,23 @@ import (
 func TestWow(t *testing.T) {
 	var v int
 	vr := NewVarReceiver(&v)
-	tok := Token(4)
-	stepShouldDone(t, vr, &tok)
+	stepShouldDone(t, vr, 4)
 	assert(t, "simple literal test", 4, v)
 
 	var v2 interface{}
 	vr = NewVarReceiver(&v2)
-	stepShouldContinue(t, vr, &Token_MapOpen)
-	stepShouldDone(t, vr, &Token_MapClose)
+	stepShouldContinue(t, vr, Token_MapOpen)
+	stepShouldDone(t, vr, Token_MapClose)
 	assert(t, "map and recurse test",
 		map[string]interface{}{},
 		v2)
 
 	var v3 interface{}
 	vr = NewVarReceiver(&v3)
-	stepShouldContinue(t, vr, &Token_MapOpen)
-	tok = "key"
-	stepShouldContinue(t, vr, &tok)
-	tok = 6
-	stepShouldContinue(t, vr, &tok)
-	stepShouldDone(t, vr, &Token_MapClose)
+	stepShouldContinue(t, vr, Token_MapOpen)
+	stepShouldContinue(t, vr, "key")
+	stepShouldContinue(t, vr, 6)
+	stepShouldDone(t, vr, Token_MapClose)
 	assert(t, "map and recurse test",
 		map[string]interface{}{"key": 6},
 		v3)
@@ -44,8 +41,8 @@ func assert(t *testing.T, title string, expect, actual interface{}) {
 	}
 }
 
-func stepShouldContinue(t *testing.T, sink TokenSink, tok *Token) {
-	done, err := sink.Step(tok)
+func stepShouldContinue(t *testing.T, sink TokenSink, tok Token) {
+	done, err := sink.Step(&tok)
 	if err != nil {
 		t.Errorf("step errored: %s", err)
 	}
@@ -54,8 +51,8 @@ func stepShouldContinue(t *testing.T, sink TokenSink, tok *Token) {
 	}
 }
 
-func stepShouldDone(t *testing.T, sink TokenSink, tok *Token) {
-	done, err := sink.Step(tok)
+func stepShouldDone(t *testing.T, sink TokenSink, tok Token) {
+	done, err := sink.Step(&tok)
 	if err != nil {
 		t.Errorf("step errored: %s", err)
 	}
