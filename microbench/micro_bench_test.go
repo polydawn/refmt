7379d113e113a8feaff984afab82abf60bd69b83
Eric Myhre <hash@exultant.us>
2016-11-01 21:01:16 -0700

    Making observations with and without GC, and with and without benchmem.
    Running in benchmem mode seems to slightly increase runtime (though not, interestingly, for bench funcs that aren't doing any allocs in the first place, which I guess makes sense).

--- microbench/micro_bench_test.go
+++ microbench/micro_bench_test.go
@@ -6,6 +6,8 @@ import (
 )
 
 // Just getting the reflect.Value of a string.
+// 45ns.
+// Or, 60ns with GC enabled.  Yes, quite a difference.
 func Benchmark_ReflectGetValueOfString(b *testing.B) {
 	var slot string
 	for i := 0; i < b.N; i++ {
@@ -16,6 +18,7 @@ func Benchmark_ReflectGetValueOfString(b *testing.B) {
 
 // This is fascinating because it's actually much faster.
 // Like 5ns compared to 60 for the direct value.
+// GC: no impact.  0 allocs (!)
 func Benchmark_ReflectGetValueOfStringRef(b *testing.B) {
 	var slot string
 	var slotAddr_rv reflect.Value
@@ -29,6 +32,7 @@ func Benchmark_ReflectGetValueOfStringRef(b *testing.B) {
 // Getting a reflect.Value of a string's address, then `Elem()`'ing back to the string type.
 // This is the hop you need to do to have an *addressable* value that you can set.
 // Like 8ns compared to the 9ns (appears `Elem()` adds ~3.5ns).
+// GC: no impact.  0 allocs (!)
 func Benchmark_ReflectGetValueOfStringRefElem(b *testing.B) {
 	var slot string
 	var slot_rav reflect.Value
@@ -40,6 +44,7 @@ func Benchmark_ReflectGetValueOfStringRefElem(b *testing.B) {
 }
 
 // About 24ns, by the same rulers as the others.
+// GC: no impact.  0 allocs (!)
 func Benchmark_ReflectSetValue(b *testing.B) {
 	var slot string
 	slot_rav := reflect.ValueOf(&slot).Elem()
@@ -52,6 +57,7 @@ func Benchmark_ReflectSetValue(b *testing.B) {
 
 // Just setting something through an address, full types.
 // Very fast (obviously): less than a single nano.
+// GC: no impact.  0 allocs (!)
 func Benchmark_DirectSetValue(b *testing.B) {
 	var slot string
 	slotAddr_v := &slot
@@ -64,6 +70,7 @@ func Benchmark_DirectSetValue(b *testing.B) {
 // Fit the address of a primitive into an `interface{}`, then type-switch
 // it back to a primitive so we can directly set it.
 // Still very fast: 2ns.
+// GC: no impact.  0 allocs (!)
 // Context: looking 4/5x faster than the equivalent ops with reflect
 // (but that's maybe less of a margin than I might've expected).
 func Benchmark_DirectInterfacedSetValue(b *testing.B) {
@@ -83,6 +90,7 @@ func Benchmark_DirectInterfacedSetValue(b *testing.B) {
 // Use a func to get that `interface{}` that's a pointer.
 // This is emulating the fastest path we can do with an atlas with user-written functions.
 // Still fast: 3ns.
+// GC: no impact.  0 allocs (!)
 func Benchmark_FuncedDirectInterfacedSetValue(b *testing.B) {
 	var slot string
 	addrFunc := func() interface{} { return &slot }
