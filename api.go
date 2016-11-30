46c734c2962412750457eb866cf81c33c377b8a7
Eric Myhre <hash@exultant.us>
2016-11-29 23:28:25 -0600

    Implement TokenPump.Run
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- api.go
+++ api.go
@@ -1,6 +1,8 @@
 package xlate
 
 import (
+	"fmt"
+
 	. "github.com/polydawn/go-xlate/tok"
 )
 
@@ -17,10 +19,24 @@ type TokenPump struct {
 	TokenSink
 }
 
-func (p TokenPump) Run() {
-	// TODO
-}
-
-func (p TokenPump) step() {
-	// TODO
+func (p TokenPump) Run() error {
+	var tok Token
+	var srcDone, sinkDone bool
+	var err error
+	for {
+		srcDone, err = p.TokenSource.Step(&tok)
+		if err != nil {
+			return err
+		}
+		sinkDone, err = p.TokenSink.Step(&tok)
+		if err != nil {
+			return err
+		}
+		if srcDone {
+			if sinkDone {
+				return nil
+			}
+			return fmt.Errorf("src at end of item but sink expects more")
+		}
+	}
 }
