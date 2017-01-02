3b9939e8d1c19e30acd64114f076724eddcc5e66
Eric Myhre <hash@exultant.us>
2017-01-02 14:17:07 +0100

    Update MarshalMachineStructAtlas test so hard that it passes now.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- obj/marshalMachineStructAtlas_test.go
+++ obj/marshalMachineStructAtlas_test.go
@@ -4,24 +4,25 @@ import (
 	"testing"
 
 	"github.com/polydawn/go-xlate/obj/atlas"
-	. "github.com/polydawn/go-xlate/testutil"
 	. "github.com/polydawn/go-xlate/tok"
 )
 
 func TestMarshalMachineStructAtlas(t *testing.T) {
 	tt := []struct {
-		title  string
-		value  interface{}
-		atlas  atlas.Atlas
-		expect []Token
+		title     string
+		targetFn  func() interface{} // func returns target, so test source looks like your call param
+		atlas     atlas.Atlas
+		expectSeq []Token
 	}{{
 		title: "struct of several primitives",
-		value: &struct {
-			X int
-			y int
-			z string
-		}{
-			1, 4, "nine",
+		targetFn: func() interface{} {
+			return &struct {
+				X int
+				Y int
+				Z string
+			}{
+				1, 4, "nine",
+			}
 		},
 		atlas: atlas.Atlas{
 			Fields: []atlas.Entry{
@@ -30,45 +31,44 @@ func TestMarshalMachineStructAtlas(t *testing.T) {
 				{Name: "z", FieldRoute: []int{2}},
 			},
 		},
-		expect: []Token{
+		expectSeq: []Token{
 			Token_MapOpen,
-			"x", 1,
-			"y", 4,
-			"z", "nine",
+			TokStr("x"), TokInt(1),
+			TokStr("y"), TokInt(4),
+			TokStr("z"), TokStr("nine"),
 			Token_MapClose,
 		},
 	}}
-	t.Skip("not done")
 	for _, tr := range tt {
 		// Setup
-		var tokens []Token
-		machine := MarshalMachineStructAtlas{
-			target: tr.value,
-			atlas:  tr.atlas,
-		}
+		tgt := tr.targetFn()
+		machine := NewMarshalMachineStructAtlas(
+			tr.atlas,
+		)
 		// Placeholders required for recursing on.
 		suite := &Suite{}
-		driver := NewMarshaler(suite, &tr.value)
+		suite.Add(tgt, machine)
+		driver := NewMarshaler(suite, tgt)
 
 		// Run steps.
 		var done bool
 		var err error
 		var tok Token
-		for n := 0; n < len(tr.expect); n++ {
+		for n, expectTok := range tr.expectSeq {
 			done, err = machine.Step(driver, suite, &tok)
-			tokens = append(tokens, tok)
+			if !IsTokenEqual(expectTok, tok) {
+				t.Errorf("step %d yielded wrong token: expected %s, got %s", n, TokenToString(expectTok), TokenToString(tok))
+			}
 			if err != nil {
 				t.Errorf("step %d (yielded %#v) errored: %s", n, tok, err)
 			}
-			if done && n != len(tr.expect)-1 {
-				t.Errorf("done early! on step %d out of %d tokens", n, len(tr.expect))
+			if done && n != len(tr.expectSeq)-1 {
+				t.Errorf("done early! on step %d out of %d tokens", n, len(tr.expectSeq))
 			}
 		}
 		if !done {
-			t.Errorf("still not done after %d tokens!", len(tr.expect))
+			t.Errorf("still not done after %d tokens!", len(tr.expectSeq))
 		}
-
-		// Check final token stream matches expectations.
-		Assert(t, tr.title, tr.expect, tokens)
+		t.Logf("test %q halted correctly and passed", tr.title)
 	}
 }
