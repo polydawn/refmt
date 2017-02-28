0b2912e23145043726c9d22075cf919398ce5042
Eric Myhre <hash@exultant.us>
2017-02-27 18:43:25 -0800

    A wild cbor decoder appears!
    The terminals are largely with the help of some code previously written by github.com/ugorji/go/codec -- also fortunately under MIT license terms, and so free and compatible with ours in this project.  The state machine stuff is all new code, since of course our token-yielding architecture is just fundamentally different.  By comparison to that library, this leaves us with a lot of interesting simplifications (or, complexities-pushed-elsewhere): we always yield uints for positive integers for example, because we have no other information (e.g. a type we know we're mapping into) to suggest we do otherwise.
    
    The reader helpers are also ugorji-inspired, though I've already made some tweaks to fix some really weird crosscutting knowledge his callers had about when zero-copy is happening, and also expect to refactor more in the future to handle errors a little more... so?
    
    Tests refer heavily to the token fixtures we extracted from the json package this morning, so that paid off nicely already.  Coverage is currently missing for many other kinds of literal terminals, but things essentially all seem to be working.
    
    Handling for definite-length arrays and maps currently absent, which is a big oversight -- that's coming up momentarily.  Support for tags also currently absent -- that one's going to take a little more doing, because it fundamentally requires more information than the tok.Token currently conveys.  (Spec'ing token fixtures for input into definite-length *encoding* tests in the future will *also* require more information out of the tok.Token, coincidentally!)  So we'll have a merry (no-so-)little refactor to tokens coming up shortly.  Hopefully all of the existing code can be migrated with sed.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ cbor/cborDecoder.go
@@ -0,0 +1,176 @@
+package cbor
+
+import (
+	"fmt"
+	"io"
+
+	. "github.com/polydawn/go-xlate/tok"
+)
+
+type Decoder struct {
+	r quickReader
+
+	stack []decoderStep // When empty, and step returns done, all done.
+	step  decoderStep   // Shortcut to end of stack.
+
+	spareBytes []byte
+}
+
+func NewDecoder(r io.Reader) (d *Decoder) {
+	d = &Decoder{
+		r:     &quickReaderStream{br: &readerByteScanner{r: r}},
+		stack: make([]decoderStep, 0, 10),
+	}
+	d.step = d.step_acceptValue
+	return
+}
+
+func (d *Decoder) Reset() {
+	d.stack = d.stack[0:0]
+	d.step = d.step_acceptValue
+}
+
+type decoderStep func(tokenSlot *Token) (done bool, err error)
+
+func (d *Decoder) Step(tokenSlot *Token) (done bool, err error) {
+	done, err = d.step(tokenSlot)
+	// If the step errored: out, entirely.
+	if err != nil {
+		return true, err
+	}
+	// If the step wasn't done, return same status.
+	if !done {
+		return false, nil
+	}
+	// If it WAS done, pop next, or if stack empty, we're entirely done.
+	nSteps := len(d.stack) - 1
+	if nSteps <= 0 {
+		return true, nil // that's all folks
+	}
+	d.step = d.stack[nSteps]
+	d.stack = d.stack[0:nSteps]
+	return false, nil
+}
+
+func (d *Decoder) pushPhase(newPhase decoderStep) {
+	d.stack = append(d.stack, d.step)
+	d.step = newPhase
+}
+
+// The original step, where any value is accepted, and no terminators for composites are valid.
+// ONLY used in the original step; all other steps handle leaf nodes internally.
+func (d *Decoder) step_acceptValue(tokenSlot *Token) (done bool, err error) {
+	majorByte := d.r.readn1()
+	return d.stepHelper_acceptValue(majorByte, tokenSlot)
+}
+
+// Step in midst of decoding an indefinite-length array.
+func (d *Decoder) step_acceptArrValueOrBreak(tokenSlot *Token) (done bool, err error) {
+	majorByte := d.r.readn1()
+	switch majorByte {
+	case cborSigilBreak:
+		*tokenSlot = Token_ArrClose
+		return true, nil
+	default:
+		_, err := d.stepHelper_acceptValue(majorByte, tokenSlot)
+		return false, err
+	}
+}
+
+// Step in midst of decoding an indefinite-length map, key expected up next.
+func (d *Decoder) step_acceptMapIndefKey(tokenSlot *Token) (done bool, err error) {
+	majorByte := d.r.readn1()
+	switch majorByte {
+	case cborSigilBreak:
+		*tokenSlot = Token_MapClose
+		return true, nil
+	default:
+		d.step = d.step_acceptMapIndefValueOrBreak
+		_, err := d.stepHelper_acceptValue(majorByte, tokenSlot) // FIXME surely not *any* value?  not composites, at least?
+		return false, err
+	}
+}
+
+// Step in midst of decoding an indefinite-length map, key expected up next.
+func (d *Decoder) step_acceptMapIndefValueOrBreak(tokenSlot *Token) (done bool, err error) {
+	majorByte := d.r.readn1()
+	switch majorByte {
+	case cborSigilBreak:
+		return true, fmt.Errorf("unexpected break; expected value in indefinite-length map")
+	default:
+		d.step = d.step_acceptMapIndefKey
+		_, err = d.stepHelper_acceptValue(majorByte, tokenSlot)
+		return false, err
+	}
+}
+
+func (d *Decoder) stepHelper_acceptValue(majorByte byte, tokenSlot *Token) (done bool, err error) {
+	switch majorByte {
+	case cborSigilNil:
+		*tokenSlot = nil
+		return true, nil
+	case cborSigilFalse:
+		*tokenSlot = false
+		return true, nil
+	case cborSigilTrue:
+		*tokenSlot = true
+		return true, nil
+	case cborSigilFloat16, cborSigilFloat32, cborSigilFloat64:
+		*tokenSlot = d.decodeFloat(majorByte)
+		return true, nil
+	case cborSigilIndefiniteBytes:
+		var x []byte
+		x, err = d.decodeBytesIndefinite(nil)
+		*tokenSlot = &x
+		return true, err
+	case cborSigilIndefiniteString:
+		var x string
+		x, err = d.decodeStringIndefinite()
+		*tokenSlot = &x
+		return true, err
+	case cborSigilIndefiniteArray:
+		*tokenSlot = Token_ArrOpen
+		d.pushPhase(d.step_acceptArrValueOrBreak)
+		return false, nil
+	case cborSigilIndefiniteMap:
+		*tokenSlot = Token_MapOpen
+		d.pushPhase(d.step_acceptMapIndefKey)
+		return false, nil
+	default:
+		switch {
+		case majorByte >= cborMajorUint && majorByte < cborMajorNegInt:
+			var x uint64
+			x, err = d.decodeUint(majorByte)
+			*tokenSlot = &x
+			return true, err
+		case majorByte >= cborMajorNegInt && majorByte < cborMajorBytes:
+			var x int64
+			x, err = d.decodeNegInt(majorByte)
+			*tokenSlot = &x
+			return true, err
+		case majorByte >= cborMajorBytes && majorByte < cborMajorString:
+			var x []byte
+			x, err = d.decodeBytes(majorByte)
+			*tokenSlot = &x
+			return true, err
+		case majorByte >= cborMajorString && majorByte < cborMajorArray:
+			var x string
+			x, err = d.decodeString(majorByte)
+			*tokenSlot = &x
+			return true, err
+		case majorByte >= cborMajorArray && majorByte < cborMajorMap:
+			*tokenSlot = Token_ArrOpen
+			d.pushPhase(d.step_acceptArrValueOrBreak) // FIXME def len tho
+			return false, nil
+		case majorByte >= cborMajorMap && majorByte < cborMajorTag:
+			*tokenSlot = Token_MapOpen
+			d.pushPhase(d.step_acceptMapIndefKey) // FIXME def len tho
+			return false, nil
+		case majorByte >= cborMajorTag && majorByte < cborMajorSimple:
+			return true, fmt.Errorf("cbor tags not supported")
+			// but when we do, it'll be by saving it as another field of the Token, and recursing.
+		default:
+			return true, fmt.Errorf("Invalid majorByte: 0x%x", majorByte)
+		}
+	}
+}
---
+++ cbor/cborDecoder_test.go
@@ -0,0 +1,184 @@
+package cbor
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	. "github.com/polydawn/go-xlate/tok"
+	"github.com/polydawn/go-xlate/tok/fixtures"
+)
+
+func bcat(bss ...[]byte) []byte {
+	l := 0
+	for _, bs := range bss {
+		l += len(bs)
+	}
+	rbs := make([]byte, 0, l)
+	for _, bs := range bss {
+		rbs = append(rbs, bs...)
+	}
+	return rbs
+}
+
+func b(b byte) []byte { return []byte{b} }
+
+func TestCborDecoder(t *testing.T) {
+	tt := []struct {
+		title    string
+		sequence fixtures.Sequence
+		input    []byte
+	}{
+		{"",
+			fixtures.SequenceMap["flat string"],
+			bcat(b(0x60+5), []byte(`value`)),
+		},
+		{"indefinite length string (single actual hunk)",
+			fixtures.SequenceMap["flat string"],
+			bcat(b(0x7f), b(0x60+5), []byte(`value`), b(0xff)),
+		},
+		{"indefinite length string (multiple hunks)",
+			fixtures.SequenceMap["flat string"],
+			bcat(b(0x7f), b(0x60+2), []byte(`va`), b(0x60+3), []byte(`lue`), b(0xff)),
+		},
+		{"",
+			fixtures.SequenceMap["strings needing escape"],
+			bcat(b(0x60+17), []byte("str\nbroken\ttabbed")),
+		},
+
+		{"indefinite length",
+			fixtures.SequenceMap["empty map"],
+			bcat(b(0xbf), b(0xff)),
+		},
+		{"indefinite length",
+			fixtures.SequenceMap["single row map"],
+			bcat(b(0xbf),
+				b(0x60+3), []byte(`key`), b(0x60+5), []byte(`value`),
+				b(0xff),
+			),
+		},
+		{"indefinite length",
+			fixtures.SequenceMap["duo row map"],
+			bcat(b(0xbf),
+				b(0x60+3), []byte(`key`), b(0x60+5), []byte(`value`),
+				b(0x60+2), []byte(`k2`), b(0x60+2), []byte(`v2`),
+				b(0xff),
+			),
+		},
+
+		{"indefinite length",
+			fixtures.SequenceMap["empty array"],
+			bcat(b(0x9f), b(0xff)),
+		},
+		{"indefinite length",
+			fixtures.SequenceMap["single entry array"],
+			bcat(b(0x9f),
+				b(0x60+5), []byte(`value`),
+				b(0xff),
+			),
+		},
+		{"indefinite length with nested indef string",
+			fixtures.SequenceMap["single entry array"],
+			bcat(b(0x9f),
+				bcat(b(0x7f), b(0x60+5), []byte(`value`), b(0xff)),
+				b(0xff),
+			),
+		},
+		{"indefinite length",
+			fixtures.SequenceMap["duo entry array"],
+			bcat(b(0x9f),
+				b(0x60+5), []byte(`value`),
+				b(0x60+2), []byte(`v2`),
+				b(0xff),
+			),
+		},
+
+		{"all indefinite length",
+			fixtures.SequenceMap["array nested in map as non-first and final entry"],
+			bcat(b(0xbf),
+				b(0x60+2), []byte(`k1`), b(0x60+2), []byte(`v1`),
+				b(0x60+2), []byte(`ke`), bcat(b(0x9f),
+					b(0x60+2), []byte(`oh`),
+					b(0x60+4), []byte(`whee`),
+					b(0x60+3), []byte(`wow`),
+					b(0xff),
+				),
+				b(0xff),
+			),
+		},
+		{"all indefinite length",
+			fixtures.SequenceMap["array nested in map as first and non-final entry"],
+			bcat(b(0xbf),
+				b(0x60+2), []byte(`ke`), bcat(b(0x9f),
+					b(0x60+2), []byte(`oh`),
+					b(0x60+4), []byte(`whee`),
+					b(0x60+3), []byte(`wow`),
+					b(0xff),
+				),
+				b(0x60+2), []byte(`k1`), b(0x60+2), []byte(`v1`),
+				b(0xff),
+			),
+		},
+		{"all indefinite length",
+			fixtures.SequenceMap["maps nested in array"],
+			bcat(b(0x9f),
+				bcat(b(0xbf),
+					b(0x60+1), []byte(`k`), b(0x60+1), []byte(`v`),
+					b(0xff),
+				),
+				b(0x60+4), []byte(`whee`),
+				bcat(b(0xbf),
+					b(0x60+2), []byte(`k1`), b(0x60+2), []byte(`v1`),
+					b(0xff),
+				),
+				b(0xff),
+			),
+		},
+		{"all indefinite length",
+			fixtures.SequenceMap["arrays in arrays in arrays"],
+			bcat(b(0x9f), b(0x9f), b(0x9f), b(0xff), b(0xff), b(0xff)),
+		},
+		{"all indefinite length",
+			fixtures.SequenceMap["maps nested in maps"],
+			bcat(b(0xbf),
+				b(0x60+1), []byte(`k`), bcat(b(0xbf),
+					b(0x60+2), []byte(`k2`), b(0x60+2), []byte(`v2`),
+					b(0xff),
+				),
+				b(0xff),
+			),
+		},
+	}
+	for _, tr := range tt {
+		// Set it up.
+		title := tr.sequence.Title
+		if tr.title != "" {
+			title = strings.Join([]string{tr.sequence.Title, tr.title}, ", ")
+		}
+		buf := bytes.NewBuffer(tr.input)
+		tokenSource := NewDecoder(buf)
+
+		// Run steps.
+		var done bool
+		var err error
+		var tok Token
+		for n, expectTok := range tr.sequence.Tokens {
+			done, err = tokenSource.Step(&tok)
+			if err != nil {
+				t.Errorf("test %q step %d (inputting %#v) errored: %s", title, n, expectTok, err)
+			}
+			if !IsTokenEqual(expectTok, tok) {
+				t.Errorf("test %q failed: step %d yielded wrong token: expected %s, got %s",
+					title, n, TokenToString(expectTok), TokenToString(tok))
+			}
+			if done && n != len(tr.sequence.Tokens)-1 {
+				t.Errorf("test %q done early! on index=%d out of %d tokens", title, n, len(tr.sequence.Tokens))
+			}
+		}
+		if !done {
+			t.Errorf("test %q still not done after %d tokens!", title, len(tr.sequence.Tokens))
+		}
+
+		t.Logf("test %q --- done", title)
+	}
+}
