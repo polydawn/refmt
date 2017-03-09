f34002228f9936953f91b3bed405b3d00556bfa2
Eric Myhre <hash@exultant.us>
2017-03-09 14:13:29 -0800

    Add top-level methods for driving cbor marshal/unmarshal.
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ helpers_cbor.go
@@ -0,0 +1,60 @@
+package xlate
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/polydawn/go-xlate/cbor"
+	"github.com/polydawn/go-xlate/obj"
+)
+
+func NewCborEncoder(wr io.Writer) *CborEncoder {
+	enc := &CborEncoder{
+		marshaller: obj.NewMarshaler(&obj.Suite{}),
+		encoder:    cbor.NewEncoder(wr),
+	}
+	enc.pump = TokenPump{
+		enc.marshaller,
+		enc.encoder,
+	}
+	return enc
+}
+
+type CborEncoder struct {
+	marshaller *obj.MarshalDriver
+	encoder    *cbor.Encoder
+	pump       TokenPump
+}
+
+func (d *CborEncoder) Marshal(v interface{}) error {
+	d.marshaller.Bind(v)
+	d.encoder.Reset()
+	return d.pump.Run()
+}
+
+func CborEncode(v interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	if err := NewCborEncoder(&buf).Marshal(v); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func NewCborDecoder(r io.Reader) *CborDecoder {
+	return &CborDecoder{r}
+}
+
+type CborDecoder struct {
+	r io.Reader
+}
+
+func (d *CborDecoder) Unmarshal(v interface{}) error {
+	return TokenPump{
+		cbor.NewDecoder(d.r),
+		obj.NewUnmarshaler(v),
+	}.Run()
+}
+
+func CborDecode(v interface{}, b []byte) error {
+	return NewCborDecoder(bytes.NewBuffer(b)).Unmarshal(v)
+}
