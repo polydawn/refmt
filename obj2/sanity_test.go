e2a9a7be2fba68f8b2b53f3666247431b3e0ce01
Eric Myhre <hash@exultant.us>
2017-04-20 00:13:30 +0200

    Quick research into detecting primitives clearly in reflection.
    True primitives should be able to exit from atlas lookups as soon as possible.  At the same time, typedefs of the primitive *kind* need to get the full treatment.
    
    Maybe on second thought this isn't surprising, but it looks like the best way to do that might actually just be a straight up enumeration switch over those known builtin primitive-sans-typedef types.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ obj2/sanity_test.go
@@ -0,0 +1,29 @@
+package obj
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestReflectInvariants(t *testing.T) {
+	describe := func(thing interface{}) {
+		rt := reflect.TypeOf(thing)
+		fmt.Printf("type: %#v (%q)\n", rt, rt)
+		fmt.Printf("kind: %#v (%q)\n", rt.Kind(), rt.Kind())
+		fmt.Printf("name: %q\n", rt.Name())
+		fmt.Printf("PkgPath: %q\n", rt.PkgPath())
+	}
+	describe("")
+	fmt.Println()
+	type StrTypedef string
+	describe(StrTypedef(""))
+
+	// I guess the cheapest and sanest way to check if something is one of the
+	// builtin primitives is just checking the rtid pointer equality outright?
+	//
+	// I think `PkgPath() == "" && (... kind is not slice, etc...)` might also work,
+	// but the documentation doesn't make it very clear if that's an intended use
+	// of the PkgPath function, and this seems much harder and less reliable
+	// than the "hack".
+}
