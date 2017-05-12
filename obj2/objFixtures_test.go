eab564012f08e35ffbf8967dc1fd039ae0f37839
Eric Myhre <hash@exultant.us>
2017-05-12 00:42:22 -0500

    Reshuffle test harness to support multiple marshal value fixtures per token fixture, as unmarshal does.
    This will be used more rarely, but it still seemed irritating to make a whole new top-level thing for it.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- obj2/objFixtures_test.go
+++ obj2/objFixtures_test.go
@@ -15,6 +15,7 @@ import (
 var skipMe = fmt.Errorf("skipme")
 
 type marshalResults struct {
+	title string
 	// Yields a value to hand to the marshaller.
 	// A func returning a wildcard is used rather than just an `interface{}`, because `&target` conveys very different type information.
 	valueFn func() interface{}
@@ -53,9 +54,10 @@ var objFixtures = []struct {
 	// The suite of mappings to use.
 	atlas atlas.Atlas
 
-	// The results expected from marshalling.  If nil: don't run marshal test.
-	// (If zero value, the result should be passing and nil errors.)
-	marshalResults *marshalResults
+	// The results to expect from various marshalling starting points.
+	// This is a slice because we occasionally have several different kinds of objects
+	// which we expect will converge on the same token fixture given the same atlas.
+	marshalResults []marshalResults
 
 	// The results to expect from various unmarshal situations.
 	// This is a slice because unmarshal may have different outcomes (usually,
@@ -64,8 +66,9 @@ var objFixtures = []struct {
 }{
 	{title: "string literal",
 		sequence: fixtures.SequenceMap["flat string"],
-		marshalResults: &marshalResults{
-			valueFn: func() interface{} { str := "value"; return str },
+		marshalResults: []marshalResults{
+			{title: "from string literal",
+				valueFn: func() interface{} { str := "value"; return str }},
 		},
 		unmarshalResults: []unmarshalResults{
 			{title: "into string",
@@ -101,8 +104,9 @@ var objFixtures = []struct {
 				AddField("X", atlas.StructMapEntry{SerialName: "key"}).
 				Complete(),
 		),
-		marshalResults: &marshalResults{
-			valueFn: func() interface{} { return tObjStr{"value"} },
+		marshalResults: []marshalResults{
+			{title: "from object with one field",
+				valueFn: func() interface{} { return tObjStr{"value"} }},
 		},
 		unmarshalResults: []unmarshalResults{
 			{title: "into string",
@@ -145,8 +149,9 @@ var objFixtures = []struct {
 				AddField("Y", atlas.StructMapEntry{SerialName: "k2"}).
 				Complete(),
 		),
-		marshalResults: &marshalResults{
-			valueFn: func() interface{} { return tObjStr2{"value", "v2"} },
+		marshalResults: []marshalResults{
+			{title: "from object with two fields",
+				valueFn: func() interface{} { return tObjStr2{"value", "v2"} }},
 		},
 		unmarshalResults: []unmarshalResults{
 			{title: "into string",
@@ -195,51 +200,56 @@ func TestMarshaller(t *testing.T) {
 	Convey("Marshaller suite:", t, func() {
 		for _, tr := range objFixtures {
 			Convey(fmt.Sprintf("%q fixture sequence:", tr.title), func() {
-				// Set up marshaller.
-				marshaller := NewMarshaler(tr.atlas)
-				marshaller.Bind(tr.marshalResults.valueFn())
+				for _, trr := range tr.marshalResults {
+					Convey(fmt.Sprintf("working %s (%T):", trr.title, trr.valueFn()), func() {
 
-				Convey("Steps...", func() {
-					// Run steps until the marshaller says done or error.
-					// For each step, assert the token matches fixtures;
-					// when error and expected one, skip token check on that step
-					// and finalize with the assertion.
-					// If marshaller doesn't stop when we expected it to based
-					// on fixture length, let it keep running three more steps
-					// so we get that much more debug info.
-					var done bool
-					var err error
-					var tok Token
-					expectSteps := len(tr.sequence.Tokens) - 1
-					for nStep := 0; nStep < expectSteps+3; nStep++ {
-						done, err = marshaller.Step(&tok)
-						if err != nil && tr.marshalResults.expectErr != nil {
-							Convey("Result (error expected)", func() {
-								So(err.Error(), ShouldResemble, tr.marshalResults.expectErr.Error())
-							})
-							return
-						}
-						if nStep <= expectSteps {
-							So(
-								step{tok.String(), err},
-								ShouldResemble,
-								step{tr.sequence.Tokens[nStep].String(), nil},
-							)
-						} else {
-							So(
-								step{tok.String(), err},
-								ShouldResemble,
-								step{Token{}.String(), fmt.Errorf("overshoot")},
-							)
-						}
-						if done {
-							Convey("Result (halted correctly)", func() {
-								So(nStep, ShouldEqual, expectSteps)
-							})
-							return
-						}
-					}
-				})
+						// Set up marshaller.
+						marshaller := NewMarshaler(tr.atlas)
+						marshaller.Bind(trr.valueFn())
+
+						Convey("Steps...", func() {
+							// Run steps until the marshaller says done or error.
+							// For each step, assert the token matches fixtures;
+							// when error and expected one, skip token check on that step
+							// and finalize with the assertion.
+							// If marshaller doesn't stop when we expected it to based
+							// on fixture length, let it keep running three more steps
+							// so we get that much more debug info.
+							var done bool
+							var err error
+							var tok Token
+							expectSteps := len(tr.sequence.Tokens) - 1
+							for nStep := 0; nStep < expectSteps+3; nStep++ {
+								done, err = marshaller.Step(&tok)
+								if err != nil && trr.expectErr != nil {
+									Convey("Result (error expected)", func() {
+										So(err.Error(), ShouldResemble, trr.expectErr.Error())
+									})
+									return
+								}
+								if nStep <= expectSteps {
+									So(
+										step{tok.String(), err},
+										ShouldResemble,
+										step{tr.sequence.Tokens[nStep].String(), nil},
+									)
+								} else {
+									So(
+										step{tok.String(), err},
+										ShouldResemble,
+										step{Token{}.String(), fmt.Errorf("overshoot")},
+									)
+								}
+								if done {
+									Convey("Result (halted correctly)", func() {
+										So(nStep, ShouldEqual, expectSteps)
+									})
+									return
+								}
+							}
+						})
+					})
+				}
 			})
 		}
 	})
