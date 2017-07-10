8395d1b1d3047459e1c4dd5e1be54d646d7a9006
Eric Myhre <hash@exultant.us>
2017-07-10 01:18:20 -0700

    Add example of building your own struct mapping atlas, with details.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- example_test.go
+++ example_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/polydawn/refmt/obj/atlas"
 )
 
-func ExampleJsonEncodeDefaults() {
+func ExampleJsonEncodeAtlasDefaults() {
 	type MyType struct {
 		X string
 		Y int
@@ -31,5 +31,33 @@ func ExampleJsonEncodeDefaults() {
 
 	// Output:
 	// {"x":"a","y":1}
+	// <nil>
+}
+
+func ExampleJsonEncodeAtlasCustom() {
+	type MyType struct {
+		X string
+		Y int
+	}
+
+	MyType_AtlasEntry := atlas.BuildEntry(MyType{}).
+		StructMap().
+		AddField("X", atlas.StructMapEntry{SerialName: "overrideName"}).
+		// and no "Y" mapping at all!
+		Complete()
+
+	atl := atlas.MustBuild(
+		MyType_AtlasEntry,
+		// this is a vararg... stack more entries here!
+	)
+
+	var buf bytes.Buffer
+	encoder := refmt.NewAtlasedJsonEncoder(&buf, atl)
+	err := encoder.Marshal(MyType{"a", 1})
+	fmt.Println(buf.String())
+	fmt.Printf("%v\n", err)
+
+	// Output:
+	// {"overrideName":"a"}
 	// <nil>
 }
