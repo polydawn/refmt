bb9bb6126d3749057546c6fe8abfe53ef9004080
Eric Myhre <hash@exultant.us>
2017-07-13 15:17:14 -0600

    Restandardize the json encoder tests to use the shared fixtures.  Correct some fixture situation labels.
    Replaces lots of previously rather redundant fixtures.  Code for the loop is now same as cbor encoder, modulo the fact we made it operate on strings instead of bytes.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- json/jsonEncoder_test.go
+++ json/jsonEncoder_test.go
@@ -6,87 +6,35 @@ import (
 	"testing"
 
 	. "github.com/polydawn/refmt/testutil"
-	"github.com/polydawn/refmt/tok/fixtures"
 )
 
 func TestJsonEncoder(t *testing.T) {
-	tt := []struct {
-		title    string
-		sequence fixtures.Sequence
-		expect   string
-	}{
-		{"",
-			fixtures.SequenceMap["flat string"],
-			`"value"`,
-		},
-		{"",
-			fixtures.SequenceMap["single row map"],
-			`{"key":"value"}`,
-		},
-		{"",
-			fixtures.SequenceMap["duo row map"],
-			`{"key":"value","k2":"v2"}`,
-		},
-		{"",
-			fixtures.SequenceMap["single entry array"],
-			`["value"]`,
-		},
-		{"",
-			fixtures.SequenceMap["duo entry array"],
-			`["value","v2"]`,
-		},
-		{"",
-			fixtures.SequenceMap["empty map"],
-			`{}`,
-		},
-		{"",
-			fixtures.SequenceMap["empty array"],
-			`[]`,
-		},
-		{"",
-			fixtures.SequenceMap["array nested in map as non-first and final entry"],
-			`{"k1":"v1","ke":["oh","whee","wow"]}`,
-		},
-		{"",
-			fixtures.SequenceMap["array nested in map as first and non-final entry"],
-			`{"ke":["oh","whee","wow"],"k1":"v1"}`,
-		},
-		{"",
-			fixtures.SequenceMap["maps nested in array"],
-			`[{"k":"v"},"whee",{"k1":"v1"}]`,
-		},
-		{"",
-			fixtures.SequenceMap["arrays in arrays in arrays"],
-			`[[[]]]`,
-		},
-		{"",
-			fixtures.SequenceMap["maps nested in maps"],
-			`{"k":{"k2":"v2"}}`,
-		},
-		{"",
-			fixtures.SequenceMap["strings needing escape"],
-			`"str\nbroken\ttabbed"`,
-		},
-	}
+	tt := jsonFixtures
+	// Loop over test table.
 	for _, tr := range tt {
+		// Skip if not tagged for encoding.
+		if tr.only&situationEncoding == 0 {
+			continue
+		}
+
 		// Set it up.
 		title := tr.sequence.Title
 		if tr.title != "" {
 			title = strings.Join([]string{tr.sequence.Title, tr.title}, ", ")
 		}
 		buf := &bytes.Buffer{}
-		sink := NewEncoder(buf)
+		tokenSink := NewEncoder(buf)
 
 		// Run steps.
 		var done bool
 		var err error
 		for n, tok := range tr.sequence.Tokens {
-			done, err = sink.Step(&tok)
+			done, err = tokenSink.Step(&tok)
 			if err != nil {
-				t.Errorf("test %q step %d (inputting %#v) errored: %s", title, n, tok, err)
+				t.Errorf("test %q step %d (inputting %s) errored: %s", title, n, tok, err)
 			}
 			if done && n != len(tr.sequence.Tokens)-1 {
-				t.Errorf("test %q done early! on step %d out of %d tokens", title, n, len(tr.sequence.Tokens))
+				t.Errorf("test %q done early! on index=%d out of %d tokens", title, n, len(tr.sequence.Tokens))
 			}
 		}
 		if !done {
@@ -94,6 +42,8 @@ func TestJsonEncoder(t *testing.T) {
 		}
 
 		// Assert final result.
-		Assert(t, title, tr.expect, buf.String())
+		Assert(t, title, tr.serial, buf.String())
+
+		t.Logf("test %q --- done", title)
 	}
 }
