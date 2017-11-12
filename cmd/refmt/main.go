6f1948a88717f4d03a46079e46ab974718db2b8a
Eric Myhre <hash@exultant.us>
2017-11-12 10:18:11 +0100

    cmd: start developing nice cli.
    previous feature gets stuffed into a subcommand... we'll have many of them; it's basically combinatoric.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- cmd/refmt/main.go
+++ cmd/refmt/main.go
@@ -4,8 +4,11 @@ import (
 	"bytes"
 	"encoding/hex"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+
+	"github.com/urfave/cli"
 
 	"github.com/polydawn/refmt/cbor"
 	"github.com/polydawn/refmt/pretty"
@@ -13,24 +16,49 @@ import (
 )
 
 func main() {
-	in, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		panic(err)
-	}
-	bs := make([]byte, len(in)/2)
-	n, err := hex.Decode(bs, in)
-	if err != nil {
-		panic(err)
-	}
-	if n != len(in)/2 {
-		panic(fmt.Errorf("hex len mismatch: %d chars became %d bytes", len(in), n))
-	}
+	os.Exit(Main(os.Args, os.Stdin, os.Stdout, os.Stderr))
+}
 
-	pump := shared.TokenPump{
-		cbor.NewDecoder(bytes.NewBuffer(bs)),
-		pretty.NewEncoder(os.Stdout),
+func Main(args []string, stdin io.Reader, stdout, stderr io.Writer) int {
+	app := cli.NewApp()
+	app.Name = "refmt"
+	app.Authors = []cli.Author{
+		cli.Author{Name: "Eric Myhre", Email: "hash@exultant.us"},
 	}
-	if err := pump.Run(); err != nil {
-		panic(err)
+	app.Commands = []cli.Command{
+		cli.Command{
+			Category: "convert",
+			Name:     "hex=cbor=pretty",
+			Usage:    "read cbor in hex, then pretty print it",
+			Action: func(c *cli.Context) error {
+				fmt.Fprintf(stderr, "hello\n")
+				in, err := ioutil.ReadAll(os.Stdin)
+				if err != nil {
+					return err
+				}
+				bs := make([]byte, len(in)/2)
+				n, err := hex.Decode(bs, in)
+				if err != nil {
+					return err
+				}
+				if n != len(in)/2 {
+					return fmt.Errorf("hex len mismatch: %d chars became %d bytes", len(in), n)
+				}
+
+				pump := shared.TokenPump{
+					cbor.NewDecoder(bytes.NewBuffer(bs)),
+					pretty.NewEncoder(os.Stdout),
+				}
+				return pump.Run()
+			},
+		},
 	}
+	app.Writer = stdout
+	app.ErrWriter = stderr
+	err := app.Run(args)
+	if err != nil {
+		fmt.Fprintf(stderr, "error: %s\n", err)
+		return 1
+	}
+	return 0
 }
