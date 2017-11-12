dd963a3dd4ff5571c199eb8c6305db3f0b66a673
Eric Myhre <hash@exultant.us>
2017-11-12 10:40:00 +0100

    cmd: more prettyprint and converters.
    This is of course easy as pie because we've set up our abstractions correctly, making these essentially cookie-cutter.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- cmd/refmt/main.go
+++ cmd/refmt/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/urfave/cli"
 
 	"github.com/polydawn/refmt/cbor"
+	"github.com/polydawn/refmt/json"
 	"github.com/polydawn/refmt/pretty"
 	"github.com/polydawn/refmt/shared"
 )
@@ -24,13 +25,46 @@ func Main(args []string, stdin io.Reader, stdout, stderr io.Writer) int {
 	}
 	app.Commands = []cli.Command{
 		cli.Command{
-			Category: "convert",
+			Category: "prettyprint",
+			Name:     "json=pretty",
+			Usage:    "read json, then pretty print it",
+			Action: func(c *cli.Context) error {
+				return shared.TokenPump{
+					json.NewDecoder(stdin),
+					pretty.NewEncoder(stdout),
+				}.Run()
+			},
+		},
+		cli.Command{
+			Category: "prettyprint",
 			Name:     "hex=cbor=pretty",
 			Usage:    "read cbor in hex, then pretty print it",
 			Action: func(c *cli.Context) error {
 				return shared.TokenPump{
 					cbor.NewDecoder(hexReader(stdin)),
 					pretty.NewEncoder(stdout),
+				}.Run()
+			},
+		},
+		cli.Command{
+			Category: "convert",
+			Name:     "json=cbor",
+			Usage:    "read json, emit equivalent cbor",
+			Action: func(c *cli.Context) error {
+				return shared.TokenPump{
+					json.NewDecoder(stdin),
+					cbor.NewEncoder(stdout),
+				}.Run()
+			},
+		},
+		cli.Command{
+			Category: "convert",
+			Name:     "hex=cbor=json",
+			Usage:    "read cbor in hex, emit equivalent json",
+			Action: func(c *cli.Context) error {
+				return shared.TokenPump{
+					cbor.NewDecoder(hexReader(stdin)),
+					json.NewEncoder(stdout),
 				}.Run()
 			},
 		},
