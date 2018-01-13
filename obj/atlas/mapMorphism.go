1aab507f2c38464d69e920f0596701f7f32da931
Eric Myhre <hash@exultant.us>
2018-01-13 19:36:44 +0100

    This builder is still desirable.
    Even if we do have the ability to set global defaults.  Exceptions to that
    might be rare, but we have all this infrastructure here already...
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- obj/atlas/mapMorphism.go
+++ obj/atlas/mapMorphism.go
@@ -1,5 +1,34 @@
 package atlas
 
+import (
+	"fmt"
+)
+
 type MapMorphism struct {
 	KeySortMode KeySortMode
+}
+
+func (x *BuilderCore) MapMorphism() *BuilderMapMorphism {
+	x.entry.MapMorphism = &MapMorphism{
+		KeySortMode_Default,
+	}
+	return &BuilderMapMorphism{x.entry}
+}
+
+type BuilderMapMorphism struct {
+	entry *AtlasEntry
+}
+
+func (x *BuilderMapMorphism) Complete() *AtlasEntry {
+	return x.entry
+}
+
+func (x *BuilderMapMorphism) SetKeySortMode(km KeySortMode) *BuilderMapMorphism {
+	switch km {
+	case KeySortMode_Default, KeySortMode_Strings, KeySortMode_RFC7049:
+		x.entry.MapMorphism.KeySortMode = km
+	default:
+		panic(fmt.Errorf("invalid key sort mode %q", km))
+	}
+	return x
 }
