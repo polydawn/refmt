369e3fec7675cc9231093fd44a444de984e55a86
Eric Myhre <hash@exultant.us>
2018-05-01 17:47:32 +0200

    testing: migrate cbor bool testcases to go-wish.
    Refraining from the 'checkCanonical' pattern used in the json package.
    Turns out that approach has some issues.  The go testing tools only
    regard your declarations of `t.Helper` up until they encounted your
    `t.Run` func in the stack... which means if you have a helper method
    calling `t.Run` itself, GAME OVER: you CANNOT have it report line
    numbers in `Logf` calls that point to the callsite above that helper.
    
    This pretty much leaves us with the same major complaint we previously
    had when doing table-driven tests: the line numbers reported for
    failures were basically useless, because the callsite is always the
    same, and all the interesting and semantically relevant data was in a
    table somewhere totally unrelated.
    
    (I both see the reasoning there, and yet am not pleased with it,
    because generating fairly standardly-shaped trees of test funcs
    and applying them is something I do *often* for interface conformance
    tests, for example, and I kinda want that experience to not suck.)
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- cbor/cborFixtures_test.go
+++ cbor/cborFixtures_test.go
@@ -4,10 +4,78 @@ import (
 	"bytes"
 	"encoding/base64"
 	"fmt"
+	"testing"
+
+	. "github.com/warpfork/go-wish"
 
 	. "github.com/polydawn/refmt/tok"
 	"github.com/polydawn/refmt/tok/fixtures"
 )
+
+func Test(t *testing.T) {
+	testBool(t)
+}
+
+func checkEncoding(t *testing.T, sequence fixtures.Sequence, expectSerial []byte, expectErr error) {
+	t.Helper()
+	outputBuf := &bytes.Buffer{}
+	tokenSink := NewEncoder(outputBuf)
+
+	// Run steps, advancing through the token sequence.
+	//  If it stops early, just report how many steps in; we Wish on that value.
+	//  If it doesn't stop in time, just report that bool; we Wish on that value.
+	var nStep int
+	var done bool
+	var err error
+	for _, tok := range sequence.Tokens {
+		nStep++
+		done, err = tokenSink.Step(&tok)
+		if done || err != nil {
+			break
+		}
+	}
+
+	// Assert final result.
+	Wish(t, done, ShouldEqual, true)
+	Wish(t, nStep, ShouldEqual, len(sequence.Tokens))
+	Wish(t, err, ShouldEqual, expectErr)
+	Wish(t, outputBuf.Bytes(), ShouldEqual, expectSerial)
+}
+
+func checkDecoding(t *testing.T, expectSequence fixtures.Sequence, serial []byte, expectErr error) {
+	// Decoding JSON is *never* going to yield length info on tokens,
+	//  so we'll strip that here rather than forcing all our fixtures to say it.
+	expectSequence = expectSequence.SansLengthInfo()
+
+	t.Helper()
+	inputBuf := bytes.NewBuffer(serial)
+	tokenSrc := NewDecoder(inputBuf)
+
+	// Run steps, advancing until the decoder reports it's done.
+	//  If the decoder keeps yielding more tokens than we expect, that's fine...
+	//  we just keep recording them, and we'll diff later.
+	//  There's a cutoff when it overshoots by 10 tokens because generally
+	//  that indicates we've found some sort of loop bug and 10 extra token
+	//  yields is typically enough info to diagnose with.
+	var nStep int
+	var done bool
+	var yield = make([]Token, len(expectSequence.Tokens)+10)
+	var err error
+	for ; nStep <= len(expectSequence.Tokens)+10; nStep++ {
+		done, err = tokenSrc.Step(&yield[nStep])
+		if done || err != nil {
+			break
+		}
+	}
+	nStep++
+	yield = yield[:nStep]
+
+	// Assert final result.
+	Wish(t, done, ShouldEqual, true)
+	Wish(t, nStep, ShouldEqual, len(expectSequence.Tokens))
+	Wish(t, yield, ShouldEqual, expectSequence.Tokens)
+	Wish(t, err, ShouldEqual, expectErr)
+}
 
 func bcat(bss ...[]byte) []byte {
 	l := 0
