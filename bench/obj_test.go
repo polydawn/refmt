4748d5ece693c4a60b401a0b21a9607fe9057775
Eric Myhre <hash@exultant.us>
2018-09-02 18:41:08 +0200

    bench: fix obj test; make corresponding map test.
    Had to rearrange the obj fixtures and types somewhat, because with maps
    and stdlib json, there's no way to define non-canonical orders -- which
    our obj fixtures were previously exercising.
    
    It's a nice little example of how much faster struct operations can be
    than maps -- a *lot*, turns out, given that you have to deal with key
    sorting, and that forces both the refmt and the stdlib implementations
    into quite a few memory allocations.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- bench/obj_test.go
+++ bench/obj_test.go
@@ -14,82 +14,82 @@ type structAlpha struct {
 	B  *structBeta
 	C  structGamma
 	C2 structGamma
+	W  string
 	X  int
 	Y  int
 	Z  string
-	W  string
 }
 type structBeta struct {
 	R *structRecursive
 }
 type structGamma struct {
-	N string
 	M int
+	N string
 }
 type structRecursive struct {
-	R *structRecursive
 	M string
+	R *structRecursive
 }
 
 var fixture_structAlpha = structAlpha{
 	&structBeta{
 		&structRecursive{
-			&structRecursive{
-				&structRecursive{},
-				"asdf",
-			},
 			"quir",
+			&structRecursive{
+				"asdf",
+				&structRecursive{},
+			},
 		},
 	},
-	structGamma{"n", 13},
-	structGamma{"n2", 14},
-	1, 2, "3", "4",
+	structGamma{13, "n"},
+	structGamma{14, "n2"},
+	"4", 1, 2, "3",
 }
 
 // note: 18 string keys, 7 string values; total 25 strings.
-var fixture_structAlpha_json = []byte(`{"B":{"R":{"R":{"R":{"R":null,"M":""},"M":"asdf"},"M":"quir"}},"C":{"N":"n","M":13},"C2":{"N":"n2","M":14},"X":1,"Y":2,"Z":"3","W":"4"}`)
-var fixture_structAlpha_cbor = []byte{0xa7, 0x61, 0x42, 0xa1, 0x61, 0x52, 0xa2, 0x61, 0x52, 0xa2, 0x61, 0x52, 0xa2, 0x61, 0x52, 0xff, 0x61, 0x4d, 0x60, 0x61, 0x4d, 0x64, 0x61, 0x73, 0x64, 0x66, 0x61, 0x4d, 0x64, 0x71, 0x75, 0x69, 0x72, 0x61, 0x43, 0xa2, 0x61, 0x4e, 0x61, 0x6e, 0x61, 0x4d, 0x0d, 0x62, 0x43, 0x32, 0xa2, 0x61, 0x4e, 0x62, 0x6e, 0x32, 0x61, 0x4d, 0x0e, 0x61, 0x58, 0x01, 0x61, 0x59, 0x02, 0x61, 0x5a, 0x61, 0x33, 0x61, 0x57, 0x61, 0x34}
+var fixture_structAlpha_json = []byte(`{"B":{"R":{"M":"quir","R":{"M":"asdf","R":{"M":"","R":null}}}},"C":{"M":13,"N":"n"},"C2":{"M":14,"N":"n2"},"W":"4","X":1,"Y":2,"Z":"3"}`)
+var fixture_structAlpha_cbor = []byte{0xa7, 0x61, 0x42, 0xa1, 0x61, 0x52, 0xa2, 0x61, 0x4d, 0x64, 0x71, 0x75, 0x69, 0x72, 0x61, 0x52, 0xa2, 0x61, 0x4d, 0x64, 0x61, 0x73, 0x64, 0x66, 0x61, 0x52, 0xa2, 0x61, 0x4d, 0x60, 0x61, 0x52, 0xf6, 0x61, 0x43, 0xa2, 0x61, 0x4d, 0x0d, 0x61, 0x4e, 0x61, 0x6e, 0x62, 0x43, 0x32, 0xa2, 0x61, 0x4d, 0x0e, 0x61, 0x4e, 0x62, 0x6e, 0x32, 0x61, 0x57, 0x61, 0x34, 0x61, 0x58, 0x01, 0x61, 0x59, 0x02, 0x61, 0x5a, 0x61, 0x33}
 var fixture_structAlpha_atlas = atlas.MustBuild(
 	atlas.BuildEntry(structAlpha{}).StructMap().
 		AddField("B", atlas.StructMapEntry{SerialName: "B"}).
 		AddField("C", atlas.StructMapEntry{SerialName: "C"}).
 		AddField("C2", atlas.StructMapEntry{SerialName: "C2"}).
+		AddField("W", atlas.StructMapEntry{SerialName: "W"}).
 		AddField("X", atlas.StructMapEntry{SerialName: "X"}).
 		AddField("Y", atlas.StructMapEntry{SerialName: "Y"}).
 		AddField("Z", atlas.StructMapEntry{SerialName: "Z"}).
-		AddField("W", atlas.StructMapEntry{SerialName: "W"}).
 		Complete(),
 	atlas.BuildEntry(structBeta{}).StructMap().
 		AddField("R", atlas.StructMapEntry{SerialName: "R"}).
 		Complete(),
 	atlas.BuildEntry(structGamma{}).StructMap().
-		AddField("N", atlas.StructMapEntry{SerialName: "N"}).
 		AddField("M", atlas.StructMapEntry{SerialName: "M"}).
+		AddField("N", atlas.StructMapEntry{SerialName: "N"}).
 		Complete(),
 	atlas.BuildEntry(structRecursive{}).StructMap().
-		AddField("R", atlas.StructMapEntry{SerialName: "R"}).
 		AddField("M", atlas.StructMapEntry{SerialName: "M"}).
+		AddField("R", atlas.StructMapEntry{SerialName: "R"}).
 		Complete(),
 )
 
 func Benchmark_StructAlpha_MarshalToCborRefmt(b *testing.B) {
 	var buf bytes.Buffer
 	exerciseMarshaller(b,
-		refmt.NewMarshaller(cbor.EncodeOptions{}, &buf), &buf,
-		fixture_arrayFlatInt, fixture_arrayFlatInt_cbor,
+		refmt.NewMarshallerAtlased(cbor.EncodeOptions{}, &buf, fixture_structAlpha_atlas), &buf,
+		fixture_structAlpha, fixture_structAlpha_cbor,
 	)
 }
 
 func Benchmark_StructAlpha_MarshalToJsonRefmt(b *testing.B) {
 	var buf bytes.Buffer
 	exerciseMarshaller(b,
-		refmt.NewMarshaller(json.EncodeOptions{}, &buf), &buf,
-		fixture_arrayFlatInt, fixture_arrayFlatInt_json,
+		refmt.NewMarshallerAtlased(json.EncodeOptions{}, &buf, fixture_structAlpha_atlas), &buf,
+		fixture_structAlpha, fixture_structAlpha_json,
 	)
 }
 
 func Benchmark_StructAlpha_MarshalToJsonStdlib(b *testing.B) {
 	exerciseStdlibJsonMarshaller(b,
-		fixture_arrayFlatInt, fixture_arrayFlatInt_json,
+		fixture_structAlpha, fixture_structAlpha_json,
 	)
 }
