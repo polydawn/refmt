ddde16a5a1e2f3d9674a9b988d7919072278653e
Eric Myhre <hash@exultant.us>
2018-10-07 17:57:18 +0200

    Made things yet faster, but still an alloc.
    And I really can't figure out that last alloc.  '-gcflags -m' isn't
    pointing out anything pleasantly obvious.  The only thing that seems
    potentially topical is...
    
    microbench/slicegrowth/slicegrowth_bench_test.go:70:11: leaking param: s
    microbench/slicegrowth/slicegrowth_bench_test.go:64:16: leaking param: s
    microbench/slicegrowth/slicegrowth_bench_test.go:64:33: leaking param: v
    
    ... but then it's also saying things like...
    
    microbench/slicegrowth/slicegrowth_bench_test.go:61:36: len(x) escapes to heap
    microbench/slicegrowth/slicegrowth_bench_test.go:61:44: cap(x) escapes to heap
    
    ... which I feel are sort of bizzare and not entirely truthy.  If that
    was true, we'd have a lot more than one alloc, wouldn't we.
    
    So there's something still to figure out here.  I don't feel inclined
    to accept that we *must* have an alloc in here.  Maybe; but ehhhh.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- microbench/slicegrowth/slicegrowth_bench_test.go
+++ microbench/slicegrowth/slicegrowth_bench_test.go
@@ -31,7 +31,7 @@ func BenchmarkReflectAppend_ConstantZero(b *testing.B) {
 	b.Logf("len, cap = %d, %d", len(x), cap(x))
 }
 
-// faster.  but still averaging an alloc per append, how and why
+// faster.  but still averaging an alloc per append, how and why.
 func BenchmarkReflectAppend_ConstZeroAndFinalSet(b *testing.B) {
 	x := []int{}
 	rv := reflect.ValueOf(&x).Elem()
@@ -44,4 +44,51 @@ func BenchmarkReflectAppend_ConstZeroAndFinalSet(b *testing.B) {
 	}
 	rv.Set(rv_moving)
 	b.Logf("x  len, cap = %d, %d", len(x), cap(x))
+}
+
+// this did still shave off another perceptible 4ns.  but not the rogue alloc.
+func BenchmarkReflectAppend_ConstZeroAndFinalSetAndNoVararg(b *testing.B) {
+	x := []int{}
+	rv := reflect.ValueOf(&x).Elem()
+	rt_val := reflect.TypeOf(0)
+	rv_valZero := reflect.Zero(rt_val)
+
+	rv_moving := rv
+	for i := 0; i < b.N; i++ {
+		rv_moving = appendOne(rv_moving, rv_valZero)
+	}
+	rv.Set(rv_moving)
+	b.Logf("x  len, cap = %d, %d", len(x), cap(x))
+}
+
+func appendOne(s reflect.Value, v reflect.Value) reflect.Value {
+	s, i0, _ := grow(s, 1)
+	s.Index(i0).Set(v)
+	return s
+}
+
+func grow(s reflect.Value, extra int) (reflect.Value, int, int) {
+	i0 := s.Len()
+	i1 := i0 + extra
+	if i1 < i0 {
+		panic("reflect.Append: slice overflow")
+	}
+	m := s.Cap()
+	if i1 <= m {
+		return s.Slice(0, i1), i0, i1
+	}
+	if m == 0 {
+		m = extra
+	} else {
+		for m < i1 {
+			if i0 < 1024 {
+				m += m
+			} else {
+				m += m / 4
+			}
+		}
+	}
+	t := reflect.MakeSlice(s.Type(), i1, m)
+	reflect.Copy(t, s)
+	return t, i0, i1
 }
