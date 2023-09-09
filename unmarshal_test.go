9394d65a69874e418b5969fbdf3555da93f1aca9
Rod Vagg <rod@vagg.org>
2023-09-09 17:37:43 +1000

    chore: add failing tests for json map comma handling cases

--- unmarshal_test.go
+++ unmarshal_test.go
@@ -16,12 +16,48 @@ func TestUnmarshal(t *testing.T) {
 			bs := []byte(`"str"`)
 			err := Unmarshal(json.DecodeOptions{}, bs, &slot)
 			So(err, ShouldBeNil)
+			So(slot, ShouldEqual, "str")
 		})
 		Convey("map", func() {
 			var slot map[string]string
 			bs := []byte(`{"x":"1"}`)
 			err := Unmarshal(json.DecodeOptions{}, bs, &slot)
 			So(err, ShouldBeNil)
+			So(slot, ShouldResemble, map[string]string{"x": "1"})
+		})
+		Convey("map comma handling", func() {
+			var slot map[string]string
+			bs := []byte(`{"x":"1","y":"2"}`)
+			err := Unmarshal(json.DecodeOptions{}, bs, &slot)
+			So(err, ShouldBeNil)
+			So(slot, ShouldResemble, map[string]string{"x": "1", "y": "2"})
+		})
+		Convey("map comma handling errors", func() {
+			for _, tc := range []struct {
+				name string
+				j    string
+				err  string
+			}{
+				// fails with: "expected colon after map key; got 0x2c"
+				{"trailing commas", `{"x":"1","y":"2",,,}`, "expected key after comma, got comma"},
+				// fails with: "expected colon after map key; got 0x2c"
+				{"just commas", `{,,,}`, "expected key after comma, got comma"},
+				// fails with: "expected colon after map key; got 0x2c"
+				{"leading commas", `{,,,"x":"1","y":"2",,,}`, "expected key after map open, got comma"},
+				// doesn't error
+				{"no commas", `{"x":"1""y":"2"}`, "expected comma after value, got quote"},
+				// doesn't error
+				{"no commas, just spaces", `{    "x":"1"    "y":"2"   }`, "expected comma after value, got quote"},
+				// doesn't error
+				{"no commas, just tabs", `{	"x":"1"	"y":"2"	}`, "expected comma after value, got quote"},
+			} {
+				Convey(tc.name, func() {
+					var slot map[string]string
+					err := Unmarshal(json.DecodeOptions{}, []byte(tc.j), &slot)
+					So(err, ShouldNotBeNil)
+					So(err.Error(), ShouldEqual, tc.err)
+				})
+			}
 		})
 	})
 }
